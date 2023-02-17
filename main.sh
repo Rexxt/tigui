dd95643e60d5b967a65f55f18568e90c43a15d82
Mizu <40831463+Rexxt@users.noreply.github.com>
2023-02-17 12:35:27 +0100

    stuff has been done

--- main.sh
+++ main.sh
@@ -18,7 +18,6 @@ UNDERLINE=$(tput smul)
 DIM=$(tput dim)
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-trap render WINCH
 tigui_scene="home"
 
 # figuring out repo path
@@ -39,23 +38,85 @@ fi
 git_name=`basename -s .git $(git config --get remote.origin.url)`
 
 # import config
-conf=""
+conf=()
 panel_number=0
 while IFS="" read -r l || [ -n "$l" ]; do
-    conf="$conf$l\n"
+    conf+=("$l")
     let panel_number++
 done < "$SCRIPT_DIR/presentation.tigui"
+
+declare -A panel_title
+panel_title["git-diff"]="diff"
+panel_title["git-log"]="commit log"
+panel_title["git-status"]="git status"
+
+get_output() {
+    case "$1" in
+        "git-diff")
+            echo $(git diff)
+        ;;
+        "git-log")
+            echo $(git log)
+        ;;
+        "git-status")
+            echo $(git status -s)
+        ;;
+    esac
+}
+
+tip_bar() {
+    case "$1" in
+        "home")
+            echo "[q]uit [c]ommit"
+        ;;
+    esac
+}
 
 render() {
     if [[ "$tigui_scene" == "home" ]]; then
         clear
         tput cup 0 0
         echo "${LIME_YELLOW}${REVERSE}TIGUI v$__TIGUI_VERSION$NORMAL ${BLUE}${REVERSE}in $BRIGHT$git_name$NORMAL"
-        echo -e "$conf$panel_number"
+
+        # calculate cols of each panel
+        local cols=$(tput cols)
+        local cols_per_panel=$(($cols/$panel_number))
+
+        # display panels
+        local panel_num=0
+        for panel in "${conf[@]}"; do
+            tput cup 1 $(($panel_num*$cols_per_panel))
+            let title=${panel_title["$panel"]}
+            echo -e "$LIME_YELLOW$REVERSE$panel_num $BRIGHT${panel_title["$panel"]}$NORMAL"
+
+            while IFS="" read -r l || [ -n "$l" ]; do
+                tput cup 2 $(($panel_num*$cols_per_panel))
+                if [[ "${#l}" -ge "$cols_per_panel" ]]; then
+                    local c=$(($cols_per_panel-1))
+                    echo "${l:0:$c}$RED|$NORMAL"
+                else
+                    echo "${l:0:$cols_per_panel}"
+                fi
+            done <<< "$(get_output $panel)"
+
+            let panel_num++
+        done
+
+        # display tip bar
+        local lines=$(tput lines)
+        local h=$(($lines-1))
+        tput cup $h 0
+        echo -n -e "$BLUE$(tip_bar $tigui_scene)$NORMAL"
     fi
 }
 
-render
 while true; do
-    :
+    render
+    read -n 1 -s act
+    case "$act" in
+        q)
+            clear
+            exit
+        ;;
+    esac
 done
